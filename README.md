20befc0a32587ca51e747868c33154ea2178ce85
Psi-Hate <Psi-Hate@users.noreply.github.com>
2020-11-06 18:51:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# oot_plugin_template
---
+++ src/mm_plugin_template/mm_plugin_template.ts
@@ -0,0 +1,23 @@
+import {IPlugin, IModLoaderAPI} from 'modloader64_api/IModLoaderAPI';
+import {IOOTCore} from 'modloader64_api/OOT/OOTAPI';
+import {InjectCore} from 'modloader64_api/CoreInjection';
+
+class oot_plugin_template implements IPlugin{
+
+    ModLoader!: IModLoaderAPI;
+    pluginName?: string | undefined;
+    @InjectCore()
+    core!: IOOTCore;
+
+    preinit(): void {
+    }
+    init(): void {
+    }
+    postinit(): void {
+    }
+    onTick(frame?: number | undefined): void {
+    }
+
+}
+
+module.exports = oot_plugin_template;
